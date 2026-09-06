e107faefc74c26b09d091164a7cbee5fd6a33ad5
Michela <m.gaeta29@studenti.unisa.it>
2026-09-06 18:27:59 +0200

    Uniformamento protocollo di comunicazione AESGCM

--- TLSSession.py
+++ TLSSession.py
@@ -1,27 +1,21 @@
 import os
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 class TLSSession:
     """Simula canale sicuro TLS tra Client e Idp"""
     def __init__(self, chiave_sessione: bytes):
         self.chiave_sessione=chiave_sessione
+        self.aesgcm =AESGCM(self.chiave_sessione)
 
     def invia_cifrato(self, plaintext: bytes)-> tuple[bytes,bytes]:
         """cifra i dati applicativi su canale TLS usando AES_CTR"""
         nonce = os.urandom(16)
-        cipher = Cipher(algorithms.AES(self.chiave_sessione), modes.CTR(nonce))
-        cifratore = cipher.encryptor()
-        ciphertext = cifratore.update(plaintext) + cifratore.finalize()
+        ciphertext = self.aesgcm.encrypt(nonce,plaintext, None)
         return nonce, ciphertext
 
     def ricevi_cifrato(self, nonce: bytes, ciphertext:bytes)->bytes:
         """Decifra i dati applicativi ricevuti"""
-        cipher = Cipher(algorithms.AES(self.chiave_sessione),modes.CTR(nonce))
-        decifratore = cipher.decryptor()
-        return decifratore.update(ciphertext)+decifratore.finalize()
+        return self.aesgcm.decrypt(nonce, ciphertext, None)
 
 
     
